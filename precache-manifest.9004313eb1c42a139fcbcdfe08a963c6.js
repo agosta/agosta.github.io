44125a3cbebf58a7769d448fa810ca3f55a8b5fd
Giovanni Agosta <agosta@acm.org>
2020-07-19 20:28:43 +0200

    deploy

---
+++ precache-manifest.9004313eb1c42a139fcbcdfe08a963c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d4d7aac9cfd016e07f",
+    "url": "/css/app.b15840e7.css"
+  },
+  {
+    "revision": "d393a10042322b70d8f8",
+    "url": "/css/chunk-vendors.bee3848c.css"
+  },
+  {
+    "revision": "4904c27c554c61f40d63828b077cc1d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d4d7aac9cfd016e07f",
+    "url": "/js/app.91b8ec45.js"
+  },
+  {
+    "revision": "d393a10042322b70d8f8",
+    "url": "/js/chunk-vendors.2215e4d9.js"
+  },
+  {
+    "revision": "ec3fd32e8d1ae37ba6c07f2cc9eda7b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
